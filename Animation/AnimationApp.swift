0064509813c9b16c5c55888d12065d6389c75422
CJ Gaspari <cjgaspari@users.noreply.github.com>
2021-10-29 00:48:53 -0700

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by CJ Gaspari on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
